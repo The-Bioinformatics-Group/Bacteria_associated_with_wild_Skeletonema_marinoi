db29c524dfc92b15ccdfcaedebae3e26c7c90a3a
mtop <mats@topel.se>
2016-04-30 11:58:41 +0200

    First commit

---
+++ 0_data/runFastq-dump.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+fastq-dump -Z SRR3201537 > SRR3201537.fastq
+fastq-dump -Z SRR3201538 > SRR3201538.fastq
+fastq-dump -Z SRR3201536 > SRR3201536.fastq
+fastq-dump -Z SRR3201535 > SRR3201535.fastq
+fastq-dump -Z SRR3201534 > SRR3201534.fastq
+fastq-dump -Z SRR3201533 > SRR3201533.fastq
+fastq-dump -Z SRR3201532 > SRR3201532.fastq
+fastq-dump -Z SRR3201530 > SRR3201530.fastq
+fastq-dump -Z SRR3201529 > SRR3201529.fastq
+fastq-dump -Z SRR3201527 > SRR3201527.fastq
+fastq-dump -Z SRR3201528 > SRR3201528.fastq
+fastq-dump -Z SRR3201524 > SRR3201524.fastq
+fastq-dump -Z SRR3201520 > SRR3201520.fastq
+fastq-dump -Z SRR3201511 > SRR3201511.fastq
