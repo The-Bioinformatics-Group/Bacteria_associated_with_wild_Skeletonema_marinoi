7174c561323b348ce005531bf1c29451f9a8d9a1
mtop <mats@topel.se>
2016-04-30 15:59:11 +0200

    Added rellative path to the input files

--- 1_PEAR/runPEAR.sh
+++ 1_PEAR/runPEAR.sh
@@ -1,17 +1,17 @@
 #!/bin/bash
 
-pear -j 12 -f f.SRR3201537.fastq -r r.SRR3201537.fastq -o SRR3201537.ASSEMBLY.fastq
-pear -j 12 -f f.SRR3201538.fastq -r r.SRR3201538.fastq -o SRR3201538.ASSEMBLY.fastq
-pear -j 12 -f f.SRR3201536.fastq -r r.SRR3201536.fastq -o SRR3201536.ASSEMBLY.fastq
-pear -j 12 -f f.SRR3201535.fastq -r r.SRR3201535.fastq -o SRR3201535.ASSEMBLY.fastq
-pear -j 12 -f f.SRR3201534.fastq -r r.SRR3201534.fastq -o SRR3201534.ASSEMBLY.fastq
-pear -j 12 -f f.SRR3201533.fastq -r r.SRR3201533.fastq -o SRR3201533.ASSEMBLY.fastq
-pear -j 12 -f f.SRR3201532.fastq -r r.SRR3201532.fastq -o SRR3201532.ASSEMBLY.fastq
-pear -j 12 -f f.SRR3201530.fastq -r r.SRR3201530.fastq -o SRR3201530.ASSEMBLY.fastq
-pear -j 12 -f f.SRR3201529.fastq -r r.SRR3201529.fastq -o SRR3201529.ASSEMBLY.fastq
-pear -j 12 -f f.SRR3201527.fastq -r r.SRR3201527.fastq -o SRR3201527.ASSEMBLY.fastq
-pear -j 12 -f f.SRR3201528.fastq -r r.SRR3201528.fastq -o SRR3201528.ASSEMBLY.fastq
-pear -j 12 -f f.SRR3201524.fastq -r r.SRR3201524.fastq -o SRR3201524.ASSEMBLY.fastq
-pear -j 12 -f f.SRR3201520.fastq -r r.SRR3201520.fastq -o SRR3201520.ASSEMBLY.fastq
-pear -j 12 -f f.SRR3201511.fastq -r r.SRR3201511.fastq -o SRR3201511.ASSEMBLY.fastq
+pear -j 12 -f ../0_data/f.SRR3201537.fastq -r ../0_data/r.SRR3201537.fastq -o SRR3201537.ASSEMBLY.fastq
+pear -j 12 -f ../0_data/f.SRR3201538.fastq -r ../0_data/r.SRR3201538.fastq -o SRR3201538.ASSEMBLY.fastq
+pear -j 12 -f ../0_data/f.SRR3201536.fastq -r ../0_data/r.SRR3201536.fastq -o SRR3201536.ASSEMBLY.fastq
+pear -j 12 -f ../0_data/f.SRR3201535.fastq -r ../0_data/r.SRR3201535.fastq -o SRR3201535.ASSEMBLY.fastq
+pear -j 12 -f ../0_data/f.SRR3201534.fastq -r ../0_data/r.SRR3201534.fastq -o SRR3201534.ASSEMBLY.fastq
+pear -j 12 -f ../0_data/f.SRR3201533.fastq -r ../0_data/r.SRR3201533.fastq -o SRR3201533.ASSEMBLY.fastq
+pear -j 12 -f ../0_data/f.SRR3201532.fastq -r ../0_data/r.SRR3201532.fastq -o SRR3201532.ASSEMBLY.fastq
+pear -j 12 -f ../0_data/f.SRR3201530.fastq -r ../0_data/r.SRR3201530.fastq -o SRR3201530.ASSEMBLY.fastq
+pear -j 12 -f ../0_data/f.SRR3201529.fastq -r ../0_data/r.SRR3201529.fastq -o SRR3201529.ASSEMBLY.fastq
+pear -j 12 -f ../0_data/f.SRR3201527.fastq -r ../0_data/r.SRR3201527.fastq -o SRR3201527.ASSEMBLY.fastq
+pear -j 12 -f ../0_data/f.SRR3201528.fastq -r ../0_data/r.SRR3201528.fastq -o SRR3201528.ASSEMBLY.fastq
+pear -j 12 -f ../0_data/f.SRR3201524.fastq -r ../0_data/r.SRR3201524.fastq -o SRR3201524.ASSEMBLY.fastq
+pear -j 12 -f ../0_data/f.SRR3201520.fastq -r ../0_data/r.SRR3201520.fastq -o SRR3201520.ASSEMBLY.fastq
+pear -j 12 -f ../0_data/f.SRR3201511.fastq -r ../0_data/r.SRR3201511.fastq -o SRR3201511.ASSEMBLY.fastq
 
