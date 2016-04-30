09ebe8c6ca2e53a404d85c376b0e27d35201e868
mtop <mats@topel.se>
2016-04-30 15:54:17 +0200

    First commit

---
+++ 0_data/runDeinterleave.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+deinterleave_fastq.sh < SRR3201537.fastq f.SRR3201537.fastq r.SRR3201537.fastq
+deinterleave_fastq.sh < SRR3201538.fastq f.SRR3201538.fastq r.SRR3201538.fastq
+deinterleave_fastq.sh < SRR3201536.fastq f.SRR3201536.fastq r.SRR3201536.fastq
+deinterleave_fastq.sh < SRR3201535.fastq f.SRR3201535.fastq r.SRR3201535.fastq
+deinterleave_fastq.sh < SRR3201534.fastq f.SRR3201534.fastq r.SRR3201534.fastq
+deinterleave_fastq.sh < SRR3201533.fastq f.SRR3201533.fastq r.SRR3201533.fastq
+deinterleave_fastq.sh < SRR3201532.fastq f.SRR3201532.fastq r.SRR3201532.fastq
+deinterleave_fastq.sh < SRR3201530.fastq f.SRR3201530.fastq r.SRR3201530.fastq
+deinterleave_fastq.sh < SRR3201529.fastq f.SRR3201529.fastq r.SRR3201529.fastq
+deinterleave_fastq.sh < SRR3201527.fastq f.SRR3201527.fastq r.SRR3201527.fastq
+deinterleave_fastq.sh < SRR3201528.fastq f.SRR3201528.fastq r.SRR3201528.fastq
+deinterleave_fastq.sh < SRR3201524.fastq f.SRR3201524.fastq r.SRR3201524.fastq
+deinterleave_fastq.sh < SRR3201520.fastq f.SRR3201520.fastq r.SRR3201520.fastq
+deinterleave_fastq.sh < SRR3201511.fastq f.SRR3201511.fastq r.SRR3201511.fastq
+
